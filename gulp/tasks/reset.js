b63f3505233eb6e30723810b0f804efc165801da
colocolov <colocolov.stas@gmail.com>
2024-12-30 16:34:05 +0200

    add ver for css/js, footer in process

--- gulp/tasks/reset.js
+++ gulp/tasks/reset.js
@@ -1,5 +1,5 @@
 import del from "del";
 export const reset = () => {
-  console.log("Paths to clean:", app.path.clean);
+  // console.log("Paths to clean:", app.path.clean);
   return del(app.path.clean);
 }
